807aeb2542162e74dac9f967aa8a5ad274b7589f
Niaina Michaël RAZAFIMANDIMBY (balzac le Geek) <michaniainar@gmail.com>
2020-02-21 13:24:34 +0300

    Fix --filter command

--- libs/FilterLib.js
+++ libs/FilterLib.js
@@ -12,23 +12,32 @@ const FilterLib = (function () {
         // var filterRegex = new RegExp(filter, "g");
 
         const filtredData = data.reduce((acc, dataItem) => {
-            const people = dataItem.people.filter(peopleItem => {
-                for (let animal of peopleItem.animals) {
-                    if (animal.name.indexOf(filter) !== -1) {
-                        return true;
-                    }
-                }
+            const people = dataItem.people.reduce((previousPeople, peopleItem) => {
+                const animals = peopleItem.animals.filter(animal => {
+                    return (animal.name.indexOf(filter) !== -1)
+                })
+
+                previousPeople = [...previousPeople, {
+                    name: peopleItem.name,
+                    animals
+                }]
+
+                return previousPeople;
+            }, [])
+
+            const filtredPeople = people.filter(peopleItem => {
+                return peopleItem.animals.length > 0
             })
 
             acc = [...acc, {
                 name: dataItem.name,
-                people
+                people: filtredPeople
             }]
 
             return acc;
         }, [])
 
-        // Remove empty key
+        // Remove element that has empty people
         return filtredData.filter(data => {
             return data.people.length > 0
         })
